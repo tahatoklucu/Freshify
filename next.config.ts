4c4833e2fcd297b28884cf63ff737d2998ce6050
Taha Toklucu <tahatoklucu@hotmail.com>
2026-07-02 22:11:50 +0300

    next config update
    next config update

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,12 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: '0fp4jdlpqlry7zs5.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 }
