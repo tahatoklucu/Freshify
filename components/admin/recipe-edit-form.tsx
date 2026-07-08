ad3d5b6306ce738d3551786fb681040147ea0b44
Taha Toklucu <tahatoklucu@hotmail.com>
2026-07-09 01:27:42 +0300

    recipe management update
    recipe management update

--- components/admin/recipe-edit-form.tsx
+++ components/admin/recipe-edit-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useTransition } from "react";
-import { updateRecipe } from "@/app/actions/admin-actions"; // Güncelleme action'ını buraya yazmalısın
+import { updateRecipe } from "@/app/actions/admin-actions";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
